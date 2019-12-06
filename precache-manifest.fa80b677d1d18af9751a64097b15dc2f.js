3946f92209e626ee5847dc2b1684859eaa59da1a
Yasser-Akeb <akebyasser.informatique@gmail.com>
2019-12-06 11:39:33 +0100

    Updates

---
+++ precache-manifest.fa80b677d1d18af9751a64097b15dc2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8f9ed82e80e2bcb854b0bad8226e1b",
+    "url": "/MyProject/index.html"
+  },
+  {
+    "revision": "26e29aa3f69971319a36",
+    "url": "/MyProject/static/css/main.b3a69b2c.chunk.css"
+  },
+  {
+    "revision": "3a3376f847ddda7f45ef",
+    "url": "/MyProject/static/js/2.48fa545d.chunk.js"
+  },
+  {
+    "revision": "26e29aa3f69971319a36",
+    "url": "/MyProject/static/js/main.4468abbf.chunk.js"
+  },
+  {
+    "revision": "4ded5a5abc8df1c3fd26",
+    "url": "/MyProject/static/js/runtime-main.edb4290f.js"
+  }
+]);
